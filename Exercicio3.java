6e040c582a46c2b8cab3cde529a01dfb963b3971
dd-henry <henry.azevedo06@gmail.com>
2024-03-25 22:16:40 -0300

    ...

---
+++ Exercicio3.java
@@ -0,0 +1,23 @@
+public class Exercicio3 {
+    public static void main(String[] args){
+        int A, B, C, D;
+        boolean Q1, Q2, Q3;
+
+        A = 5;
+        B = 4;
+        C = 3;
+        D = 6;
+
+        Q1 = (A > C) && (C <= D);
+        Q2 = (A+B) > 10 || (A+B) == (C+D);
+        Q3 = (A>=C) && (D >= C) ;
+
+
+        System.out.println("A = 5    B = 4    C = 3    D = 6");
+        System.out.println("(A > C) && (C <= D)   -->  (" + Q1 + ")");
+        System.out.println("(A + B) > 10 || (A + B) == (C + D)   -->  (" + Q2 + ")");
+        System.out.println("(A >= C) && (D >= C)   -->  (" + Q3 + ")");
+
+
+    }
+}
